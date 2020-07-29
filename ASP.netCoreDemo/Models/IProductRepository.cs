b6bec22a361238a56b3f56c2ca0f28e9471138ad
Matt Brogan <broganmatt11@gmail.com>
2020-07-29 09:54:27 -0400

    demo complete, adding BootStrap

---
+++ ASP.netCoreDemo/Models/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.netCoreDemo.Models
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Products> GetAllProducts();
+        public Products GetProduct(int id);
+        public void UpdateProduct(Products product);
+        public void InsertProduct(Products productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Products AssignProducts();
+        public void DeleteProduct(Products product);
+    }
+}
---
+++ ASP.netCoreDemo/Models/ProductRepository.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.netCoreDemo.Models
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public IEnumerable<Products> GetAllProducts()
+        {
+            return _conn.Query<Products>("SELECT * FROM PRODUCTS");
+        }
+
+        public Products GetProduct(int id)
+        {
+            return (Products)_conn.QuerySingle<Products>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+                new { id = id });
+
+        }
+
+        public void UpdateProduct(Products product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
+                new { name = product.Name, price = product.Price, id = product.ProductID });
+
+        }
+
+        public void InsertProduct(Products productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+
+        }
+
+        public Products AssignProducts()
+        {
+            var categoryList = GetCategories();
+            var product = new Products();
+            product.Categories = categoryList;
+
+            return product;
+
+        }
+
+        public void DeleteProduct(Products product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;",
+                                                  new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+        }
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+    }
+}
